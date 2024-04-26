fb6949220eaf7aa5ad1766a7351db1ae8677adc8
George Stephanis <daljo628@gmail.com>
2024-04-26 15:32:41 -0400

    Add 'company' to addresses without a $ prefix for custom field.

--- src/inc/woocommerce-actions.php
+++ src/inc/woocommerce-actions.php
@@ -557,6 +557,7 @@ class Events {
 		// Missing parameters -- company, email (For billing, not shipping)
 		return array(
 			'$name'      => $address['first_name'] . ' ' . $address['last_name'],
+			'company'    => $address['company'],
 			'$phone'     => $address['phone'],
 			'$address_1' => $address['address_1'],
 			'$address_2' => $address['address_2'],
@@ -594,6 +595,7 @@ class Events {
 		// Missing parameters -- company, email (For billing, not shipping)
 		return array(
 			'$name'      => $address['first_name'] . ' ' . $address['last_name'],
+			'company'    => $address['company'],
 			'$phone'     => $address['phone'],
 			'$address_1' => $address['address_1'],
 			'$address_2' => $address['address_2'],
