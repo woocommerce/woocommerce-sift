306d00c8784558e3e53365e003485c36615768e9
Christopher Smith <chris@chriswsmith.info>
2024-10-30 12:58:31 -0500

    Make decisions use actions instead of functions

--- src/inc/abuse-decisions.php
+++ src/inc/abuse-decisions.php
@@ -1,54 +1,46 @@
 <?php
 
-namespace WPCOMSpecialProjects\SiftDecisions\Abuse_Decisions;
 
-use function WPCOMSpecialProjects\SiftDecisions\Abuse_Decision_Actions\{
-	unblock_user_from_purchases,
-	void_and_refund_user_orders,
-	cancel_and_remove_user_subscriptions,
-	remove_user_licenses_and_product_keys,
-	display_sgdc_error,
-	force_user_logout
-};
+namespace WPCOMSpecialProjects\SiftDecisions\Abuse_Decisions;
 
 /**
  * Process the Sift decision received.
  *
  * @param mixed   $return_value The return value.
  * @param string  $decision_id  The ID of the Sift decision.
- * @param integer $entity_id    The ID of the entity the decision is corrosponds to.
+ * @param integer $entity_id    The ID of the entity the decision corresponds to.
  *
  * @return mixed
  */
 function process_sift_decision_received( $return_value, $decision_id, $entity_id ) {
-
+	// We can rename once we have a final name.
 	switch ( $decision_id ) {
 		case 'trust_list_payment_abuse':
-			handle_trust_list_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_trust_list_payment_abuse', $entity_id );
 			break;
 
 		case 'looks_good_payment_abuse':
-			handle_looks_good_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_looks_good_payment_abuse', $entity_id );
 			break;
 
 		case 'not_likely_fraud_payment_abuse':
-			handle_not_likely_fraud_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_not_likely_fraud_payment_abuse', $entity_id );
 			break;
 
 		case 'likely_fraud_refundno_renew_payment_abuse':
-			handle_likely_fraud_refundno_renew_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_likely_fraud_refundno_renew_payment_abuse', $entity_id );
 			break;
 
 		case 'likely_fraud_keep_purchases_payment_abuse':
-			handle_likely_fraud_keep_purchases_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_likely_fraud_keep_purchases_payment_abuse', $entity_id );
 			break;
 
 		case 'fraud_payment_abuse':
-			handle_fraud_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_fraud_payment_abuse', $entity_id );
 			break;
 
 		case 'block_wo_review_payment_abuse':
-			handle_block_wo_review_payment_abuse( $entity_id );
+			do_action( 'sift_decisions_sift_action_block_wo_review_payment_abuse', $entity_id );
 			break;
 
 		case 'looks_ok_payment_abuse':
@@ -62,90 +54,3 @@ function process_sift_decision_received( $return_value, $decision_id, $entity_id
 	return $return_value;
 }
 add_filter( 'sift_decision_received', __NAMESPACE__ . '\process_sift_decision_received', 10, 5 );
-
-/**
- * Handle the 'trust_list_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_trust_list_payment_abuse( $user_id ) {
-	unblock_user_from_purchases( $user_id );
-}
-
-/**
- * Handle the 'looks_good_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_looks_good_payment_abuse( $user_id ) {
-	unblock_user_from_purchases( $user_id );
-}
-
-/**
- * Handle the 'not_likely_fraud_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_not_likely_fraud_payment_abuse( $user_id ) {
-	unblock_user_from_purchases( $user_id );
-}
-
-/**
- * Handle the 'likely_fraud_refundno_renew_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_likely_fraud_refundno_renew_payment_abuse( $user_id ) {
-	update_user_meta( $user_id, 'is_blocked_from_purchases', true );
-	void_and_refund_user_orders( $user_id );
-	cancel_and_remove_user_subscriptions( $user_id );
-	remove_user_licenses_and_product_keys( $user_id );
-	display_sgdc_error( 'You are blocked from making purchases due to a recent fraud review. SGDC Error OYBPXRQ' );
-}
-
-/**
- * Handle the 'likely_fraud_keep_purchases_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_likely_fraud_keep_purchases_payment_abuse( $user_id ) {
-	update_user_meta( $user_id, 'is_blocked_from_purchases', true );
-	display_sgdc_error( 'You are blocked from making purchases due to a recent fraud review. SGDC Error OYBPXRQ' );
-}
-
-/**
- * Handle the 'fraud_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_fraud_payment_abuse( $user_id ) {
-	update_user_meta( $user_id, 'is_blocked_from_purchases', true );
-	void_and_refund_user_orders( $user_id );
-	cancel_and_remove_user_subscriptions( $user_id );
-	remove_user_licenses_and_product_keys( $user_id );
-	display_sgdc_error( 'You are blocked from making purchases due to fraudulent activity. SGDC Error OYBPXRQ' );
-	force_user_logout( $user_id );
-}
-
-/**
- * Handle the 'block_wo_review_payment_abuse' decision.
- *
- * @param integer $user_id The ID of the user.
- *
- * @return void
- */
-function handle_block_wo_review_payment_abuse( $user_id ) {
-	update_user_meta( $user_id, 'is_blocked_from_purchases', true );
-}
---
+++ src/inc/wccom-sidecar.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace WPCOMSpecialProjects\SiftDecisions\WCCOM_Sidecar;
+
+use function WPCOMSpecialProjects\SiftDecisions\Abuse_Decision_Actions\{
+	unblock_user_from_purchases,
+	void_and_refund_user_orders,
+	cancel_and_remove_user_subscriptions,
+	remove_user_licenses_and_product_keys,
+	display_sgdc_error,
+	force_user_logout
+};
+
+// Hook each action to the appropriate function from Abuse_Decision_Actions.
+// We can rename once we have a final name.
+add_action( 'sift_for_woo_sift_action_trust_list_payment_abuse', __NAMESPACE__ . '\unblock_user_from_purchases', 10, 1 );
+add_action( 'sift_for_woo_sift_action_looks_good_payment_abuse', __NAMESPACE__ . '\unblock_user_from_purchases', 10, 1 );
+add_action( 'sift_for_woo_sift_action_not_likely_fraud_payment_abuse', __NAMESPACE__ . '\unblock_user_from_purchases', 10, 1 );
+
+add_action(
+	'sift_for_woo_sift_action_likely_fraud_refundno_renew_payment_abuse',
+	function ( $user_id ) {
+		update_user_meta( $user_id, 'is_blocked_from_purchases', true );
+		void_and_refund_user_orders( $user_id );
+		cancel_and_remove_user_subscriptions( $user_id );
+		remove_user_licenses_and_product_keys( $user_id );
+		display_sgdc_error( 'You are blocked from making purchases due to a recent fraud review. SGDC Error OYBPXRQ' );
+	},
+	10,
+	1
+);
+
+add_action(
+	'sift_for_woo_sift_action_likely_fraud_keep_purchases_payment_abuse',
+	function ( $user_id ) {
+		update_user_meta( $user_id, 'is_blocked_from_purchases', true );
+		display_sgdc_error( 'You are blocked from making purchases due to a recent fraud review. SGDC Error OYBPXRQ' );
+	},
+	10,
+	1
+);
+
+add_action(
+	'sift_for_woo_sift_action_fraud_payment_abuse',
+	function ( $user_id ) {
+		update_user_meta( $user_id, 'is_blocked_from_purchases', true );
+		void_and_refund_user_orders( $user_id );
+		cancel_and_remove_user_subscriptions( $user_id );
+		remove_user_licenses_and_product_keys( $user_id );
+		display_sgdc_error( 'You are blocked from making purchases due to fraudulent activity. SGDC Error OYBPXRQ' );
+		force_user_logout( $user_id );
+	},
+	10,
+	1
+);
+
+add_action(
+	'sift_for_woo_sift_action_block_wo_review_payment_abuse',
+	function ( $user_id ) {
+		update_user_meta( $user_id, 'is_blocked_from_purchases', true );
+	},
+	10,
+	1
+);
