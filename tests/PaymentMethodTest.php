8d08c37bea94d43dbfc4816e08ce24dd078ad660
Chris McCluskey <chris.mccluskey@automattic.com>
2024-11-12 15:58:36 -0800

    Add card bin test

--- tests/PaymentMethodTest.php
+++ tests/PaymentMethodTest.php
@@ -23,7 +23,7 @@ class PaymentMethodTest extends WP_UnitTestCase {
 	/**
 	 * Test getting the card_last4 property.
 	 *
-	 * @dataProvider card_last4_provider
+	 * @dataProvider get_card_last4_provider
 	 *
 	 * @param Payment_Gateway $gateway  The payment gateway in use.
 	 * @param mixed           $data     The data which contains the card_last4 value.
@@ -41,7 +41,7 @@ class PaymentMethodTest extends WP_UnitTestCase {
 	 *
 	 * @return array An array of test runs and the data associated with each run.
 	 */
-	public function card_last4_provider(): array {
+	public function get_card_last4_provider(): array {
 		$stripe_card_last4     = '4242';
 		$transact_card_last4   = '1111';
 		$stripe_payment_method = (object) array(
@@ -69,6 +69,52 @@ class PaymentMethodTest extends WP_UnitTestCase {
 				'gateway'  => $transact_gateway,
 				'data'     => $mock_order,
 				'expected' => $transact_card_last4,
+			),
+		);
+	}
+
+	/**
+	 * Test getting the card_bin property.
+	 *
+	 * @dataProvider get_card_bin_provider
+	 *
+	 * @param Payment_Gateway $gateway  The payment gateway in use.
+	 * @param mixed           $data     The data which contains the card_bin value.
+	 * @param null|string     $expected The expected result if available.
+	 *
+	 * @return void
+	 */
+	public function test_get_card_bin( Payment_Gateway $gateway, mixed $data, ?string $expected ) {
+		$result = Payment_Method::get_card_bin( $gateway, $data );
+		$this->assertEquals( $expected, $result, 'Should return the expected result' );
+	}
+
+	/**
+	 * Provide data to the test_get_card_bin test function.
+	 *
+	 * @return array An array of test runs and the data associated with each run.
+	 */
+	public function get_card_bin_provider(): array {
+		$stripe_card_bin       = '4242';
+		$stripe_payment_method = (object) array(
+			'card' => (object) array(
+				'iin' => $stripe_card_bin,
+			),
+		);
+		$order                 = new \WC_Order();
+
+		$stripe_gateway   = new Payment_Gateway( 'stripe', $order );
+		$transact_gateway = new Payment_Gateway( 'transact', $order );
+		return array(
+			'Stripe\'s object returns the card_bin property' => array(
+				'gateway'  => $stripe_gateway,
+				'data'     => $stripe_payment_method,
+				'expected' => $stripe_card_bin,
+			),
+			'Transact\'s object does not return the card_bin property' => array(
+				'gateway'  => $transact_gateway,
+				'data'     => $order,
+				'expected' => '',
 			),
 		);
 	}
