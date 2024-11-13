c241208528b0e7fd9c32fa231afa6002a4c891df
Chris McCluskey <chris.mccluskey@automattic.com>
2024-11-13 14:12:54 -0800

    Try increasing test coverage

--- tests/PaymentMethodTest.php
+++ tests/PaymentMethodTest.php
@@ -18,6 +18,21 @@ require_once __DIR__ . '/../src/inc/payment-type.php';
  */
 class PaymentMethodTest extends WP_UnitTestCase {
 
+	protected function build_mock_stripe_payment_method_object( array $config, bool $as_array = false ) {
+		if ( $as_array ) {
+			return [
+				'card' => [
+					'last4' => $config['last4'] ?? '0000',
+				],
+			];
+		}
+		return (object) [
+			'card' => (object) [
+				'last4' => $config['last4'] ?? '4242',
+			],
+		];
+	}
+
 	/**
 	 * Test getting the card_last4 property.
 	 *
@@ -42,10 +57,17 @@ class PaymentMethodTest extends WP_UnitTestCase {
 	public function get_card_last4_provider(): array {
 		$stripe_card_last4     = '4242';
 		$transact_card_last4   = '1111';
-		$stripe_payment_method = (object) array(
-			'card' => (object) array(
+		$woocommerce_payments_card_last4 = '4001';
+		$stripe_payment_method = $this->build_mock_stripe_payment_method_object(
+			[
 				'last4' => $stripe_card_last4,
-			),
+			]
+		);
+		$woocommerce_payments_payment_method = $this->build_mock_stripe_payment_method_object(
+			[
+				'last4' => $woocommerce_payments_card_last4,
+			],
+			true
 		);
 		$mock_order            = $this->getMockBuilder( \WC_Order::class )
 			->disableOriginalConstructor()
@@ -55,8 +77,11 @@ class PaymentMethodTest extends WP_UnitTestCase {
 			->method( 'get_meta' )
 			->willReturn( $transact_card_last4 );
 
+		add_filter( 'sift_for_woocommerce_woocommerce_payments_payment_method_details_from_order', fn() => $woocommerce_payments_payment_method, 1 );
+
 		$stripe_gateway   = new Payment_Gateway( 'stripe', $mock_order );
 		$transact_gateway = new Payment_Gateway( 'transact', $mock_order );
+		$woocommerce_payments_gateway = new Payment_Gateway( 'woocommerce_payments', $mock_order );
 		return array(
 			'Stripe\'s object returns the card_last4 property' => array(
 				'gateway'  => $stripe_gateway,
@@ -68,6 +93,11 @@ class PaymentMethodTest extends WP_UnitTestCase {
 				'data'     => $mock_order,
 				'expected' => $transact_card_last4,
 			),
+			'WooCommerce Payments\'s object returns the card_last4 property' => [
+				'gateway'  => $woocommerce_payments_gateway,
+				'data'     => $mock_order,
+				'expected' => $woocommerce_payments_card_last4,
+			]
 		);
 	}
 
