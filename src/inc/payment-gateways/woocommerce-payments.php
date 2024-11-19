171ad66c1e7c6ea560993079bef658306ae29684
Chris McCluskey <chris@mccluskey.us>
2024-11-19 14:37:12 -0800

    Fix payment_methods parameter (#12)
    * Fix payment_methods parameter
    
    * Update tests
    
    * Delint

--- src/inc/payment-gateways/woocommerce-payments.php
+++ src/inc/payment-gateways/woocommerce-payments.php
@@ -13,42 +13,52 @@ add_filter(
 	'sift_for_woocommerce_woocommerce_payments_payment_method_details_from_order',
 	function ( $value, \WC_Order $order ) {
 		try {
-			$charge_id  = \WC_Payments::get_order_service()->get_charge_id_for_order( $order );
+			$charge_id = \WC_Payments::get_order_service()->get_charge_id_for_order( $order );
+			if ( empty( $charge_id ) ) {
+				return $value;
+			}
 			$api_client = \WC_Payments::get_payments_api_client();
 			$charge     = $api_client->get_charge( $charge_id );
-			return $charge->get_payment_method_details();
+			return $charge['payment_method_details'];
 		} catch ( \Exception ) {
 			return $value;
 		}
-	}
+	},
+	10,
+	2
 );
 
 add_filter(
 	'sift_for_woocommerce_woocommerce_payments_charge_details_from_order',
 	function ( $value, \WC_Order $order ) {
 		try {
-			$charge_id  = \WC_Payments::get_order_service()->get_charge_id_for_order( $order );
+			$charge_id = \WC_Payments::get_order_service()->get_charge_id_for_order( $order );
+			if ( empty( $charge_id ) ) {
+				return $value;
+			}
 			$api_client = \WC_Payments::get_payments_api_client();
 			return $api_client->get_charge( $charge_id );
 		} catch ( \Exception ) {
 			return $value;
 		}
-	}
+	},
+	10,
+	2
 );
 
 add_filter( 'sift_for_woocommerce_woocommerce_payments_payment_type_string', array( \Sift_For_WooCommerce\PaymentGateways\Lib\Stripe::class, 'convert_payment_type_to_sift_payment_type' ) );
 add_filter( 'sift_for_woocommerce_woocommerce_payments_card_last4', fn( $value, $woocommerce_payments_payment_method_details ) => $woocommerce_payments_payment_method_details['card']['last4'] ?? $value, 10, 2 );
 add_filter( 'sift_for_woocommerce_woocommerce_payments_card_bin', fn( $value, $woocommerce_payments_payment_method_details ) => $woocommerce_payments_payment_method_details['card']['iin'] ?? $value, 10, 2 );
 
-add_filter( 'sift_for_woocommerce_woocommerce_payments_cvv_result_code', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['checks']['cvc_check'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_sepa_direct_debit_mandate', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['sepa_debit']['mandate'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_wallet_type', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['wallet']['type'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_cvv_result_code', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['checks']['cvc_check'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_sepa_direct_debit_mandate', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['sepa_debit']['mandate'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_wallet_type', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['wallet']['type'] ?? $value, 10, 2 );
 
-add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_cvc_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['checks']['cvc_check'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_address_line1_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['checks']['address_line1_check'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_address_zip_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['checks']['address_postal_code_check'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_funding', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['funding'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_brand', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge->get_payment_method_details()['card']['brand'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_cvc_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['checks']['cvc_check'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_address_line1_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['checks']['address_line1_check'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_address_zip_check', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['checks']['address_postal_code_check'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_funding', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['funding'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_woocommerce_payments_stripe_brand', fn( $value, $woocommerce_payments_charge ) => $woocommerce_payments_charge['payment_method_details']['card']['brand'] ?? $value, 10, 2 );
 
 /**
  * Processes events (from Stripe and more) before sending the data elsewhere.
