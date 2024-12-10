ec1cf95fc950dd5bed5450f411c5a55ad1bd44bf
Chris McCluskey <chris@mccluskey.us>
2024-12-10 10:22:39 -0800

    Add customer payment methods and PPCP chargeback reporting (#25)
    * Add customer payment methods
    
    * Use singular payment method and remove empty values
    
    * Return an empty array so that it passes validation
    
    * Delint
    
    * Allow enumeration
    
    * Try using get_current_user_id when sourcing user_id
    
    * Remove payment_method if empty
    
    * Try to source the user id from order if returned user is admin
    
    * Use user id from order when is_admin
    
    * Try using session id in this case
    
    * Add debugging for woocommerce-payments integration
    
    * Add more temporary debug statements
    
    * Add more temp debug logging
    
    * Fix debugging bug
    
    * Log events as they are sent to Sift
    
    * Try to find missing event with temp debug logs
    
    * Use user id from order if it isn't otherwise available
    
    * Add debugging logs for getting customer payment methods
    
    * Use array filter instead of array reduce
    
    * Use array_reduce correctly
    
    * Seed the array_reduce function correctly
    
    * Cache customer orders
    
    * Remove debug logs
    
    * Remove log from woocommerce-payments integration
    
    * Promote order cache to singleton
    
    * Get payments from purchase-unit in order instead of purchase unit from factory
    
    * Get name from objects
    
    * Fix typo
    
    * Use correct property from object
    
    * Remove use of session unique id for user_id
    
    * Add chargeback event to PPCP
    
    * Remove unused variable
    
    * Prevent errors
    
    * Remove action for creating chargeback with ppcp gateway and address feedback
    
    * Delint

--- src/inc/payment-gateways/ppcp-gateway.php
+++ src/inc/payment-gateways/ppcp-gateway.php
@@ -4,6 +4,7 @@ namespace Sift_For_WooCommerce\PaymentGateways\PPCP;
 
 use WooCommerce\PayPalCommerce\PPCP;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayPalGateway;
+use Sift_For_WooCommerce\Sift_Events\Events;
 
 add_filter( 'sift_for_woocommerce_ppcp-gateway_payment_gateway_string', fn() => '$paypal' );
 
@@ -44,13 +45,71 @@ add_filter( 'sift_for_woocommerce_ppcp-gateway_charge_details_from_order', __NAM
 add_filter( 'sift_for_woocommerce_ppcp-gateway_payment_type_string', fn( $value, $ppcp_gateway_payment_type ) => 'ppcp' === $ppcp_gateway_payment_type ? '$third_party_processor' : $value );
 
 add_filter( 'sift_for_woocommerce_ppcp-gateway_card_last4', fn( $value, $ppcp_data ) => $ppcp_data['wc_order']?->get_meta_data( PayPalGateway::FRAUD_RESULT_META_KEY )['card_last_digits'] ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_ppcp-gateway_avs_result_code', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->authorizations()[0]?->fraud_processor_response()->avs_code() ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_ppcp-gateway_cvv_result_code', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->authorizations()[0]?->fraud_processor_response()->cvv_code() ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_ppcp-gateway_verification_status', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->authorizations()[0]?->status() ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_ppcp-gateway_decline_reason_code', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->authorizations()[0]?->to_array()['reason_code'] ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_avs_result_code', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->authorizations()[0]?->fraud_processor_response()->avs_code() ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_cvv_result_code', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->authorizations()[0]?->fraud_processor_response()->cvv_code() ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_verification_status', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->authorizations()[0]?->status()->name() ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_decline_reason_code', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->authorizations()[0]?->to_array()['reason_code'] ?? $value, 10, 2 );
 
 add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_payer_id', fn( $value, $ppcp_data ) => $ppcp_data['order']?->payer()?->payer_id() ?? $value, 10, 2 );
 add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_payer_email', fn( $value, $ppcp_data ) => $ppcp_data['order']?->payer()?->email_address() ?? $value, 10, 2 );
 
-add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_protection_eligibility', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->captures()[0]->seller_protection() ?? $value, 10, 2 );
-add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_payment_status', fn( $value, $ppcp_data ) => $ppcp_data['purchase-unit']?->payments()?->captures()[0]->status()->name() ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_protection_eligibility', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->captures()[0]?->seller_protection()?->status ?? $value, 10, 2 );
+add_filter( 'sift_for_woocommerce_ppcp-gateway_paypal_payment_status', fn( $value, $ppcp_data ) => $ppcp_data['order']?->purchase_units()[0]?->payments()?->captures()[0]->status()->name() ?? $value, 10, 2 );
+
+/**
+ * Send a chargeback event to Sift.
+ *
+ * @param object $event The Stripe event object.
+ *
+ * @return void
+ */
+function send_chargeback_to_sift( $event ): void {
+	$order_id = $event['data']['object']['resource']['disputed_transactions'][0]['seller_transaction_id'] ?? null;
+	// Log the resolved order ID.
+	if ( ! $order_id ) {
+		wc_get_logger()->error( 'Order ID not found in event.' );
+		return;
+	}
+	$order = wc_get_order( $order_id );
+
+	if ( ! $order instanceof \WC_Order ) {
+		wc_get_logger()->error( 'WooCommerce order not found for Order ID: ' . esc_html( $order_id ) );
+		return;
+	}
+
+	$chargeback_reason = convert_dispute_reason_to_sift_chargeback_reason( $event['data']['object']['resource']['reason'] ?? '' );
+
+	Events::chargeback( $order_id, $order, $chargeback_reason );
+}
+
+/**
+ * Convert a dispute reason from a string that PayPal would use to a string that Sift would use.
+ *
+ * @param string $dispute_reason A dispute reason string that PayPal would use.
+ *
+ * @return string|null A dispute reason string that Sift would use.
+ */
+function convert_dispute_reason_to_sift_chargeback_reason( string $dispute_reason ): ?string {
+	switch ( $dispute_reason ) {
+		case 'MERCHANDISE_OR_SERVICE_NOT_RECEIVED':
+			return '$product_not_received';
+		case 'MERCHANDISE_OR_SERVICE_NOT_AS_DESCRIBED':
+			return '$product_unacceptable';
+		case 'UNAUTHORIZED':
+			return '$authorization';
+		case 'PROBLEM_WITH_REMITTANCE':
+			return '$processing_errors';
+		case 'DUPLICATE_TRANSACTION':
+			return '$duplicate';
+		case 'INCORRECT_AMOUNT':
+		case 'CREDIT_NOT_PROCESSED':
+		case 'PAYMENT_BY_OTHER_MEANS':
+			return '$customer_disputes';
+		case 'CANCELED_RECURRING_BILLING':
+			return '$cancel_subscription';
+		case 'OTHER':
+			return '$other';
+		default:
+			return null;
+	}
+}
